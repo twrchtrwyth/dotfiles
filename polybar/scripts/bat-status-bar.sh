976f49e5f9ae37c62a53c2e3a73b2c1beff10bd7
Wil Ifan Evans <w.evans@posteo.net>
2025-03-03 22:36:22 +0000

    Fix dodgy Polybar battery script.

--- polybar/scripts/bat-status-bar.sh
+++ polybar/scripts/bat-status-bar.sh
@@ -22,37 +22,26 @@ fi
 # Get the AC adapter status
 ac_status=$(cat /sys/class/power_supply/AC/online)
 
-# This is a hacky mess
-if [ -d /sys/class/power_supply/BAT1/ ]; then # Slice battery connected
+# Show stats for the slice battery if connected and charged
+if [ -d /sys/class/power_supply/BAT1/ ] && [ "$bat1" -gt 0 ]; then
   if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
     echo "%{F#1ABB9B}BATRI%{F-} +$bat1%" 
   elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
     echo "%{F#1ABB9B}BATRI%{F-} LLAWN"
   elif [ "$bat1" -lt 16 ]; then
-    while [ "$bat1" -lt 16 ]
-    do
-      echo "%{F#BC4028}! BATRI ISEL%{F-} $bat1%"
-      sleep 0.5s
-      echo "%{F#BC4028}BATRI ISEL%{F-} $bat1%"
-      sleep 0.5s
-    done
+    echo "%{F#BC4028}BATRI ISEL%{F-} +$bat1%"
   else
-    echo "%{F#1ABB9B}BATRI%{F-} -$bat1%"
+    echo "%{F#1ABB9B}BATRI%{F-} +$bat1%" 
   fi
-else # If only internal battery in use
+# Otherwise just show status of internal battery
+else
   if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
     echo "%{F#1ABB9B}BATRI%{F-} +$bat0%" 
   elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
     echo "%{F#1ABB9B}BATRI%{F-} LLAWM"
   elif [ "$bat0" -lt 16 ]; then
-    while [ "$bat0" -lt 16 ]
-    do
-      echo "%{F#BC4028}! BATRI ISEL%{F-} $bat0%"
-      sleep 0.5s
-      echo "%{F#BC4028}BATRI ISEL%{F-} $bat0%"
-      sleep 0.5s
-    done
+    echo "%{F#BC4028}BATRI ISEL%{F-} +$bat0%"
   else
-    echo "%{F#1ABB9B}BATRI%{F-} -$bat0%"
+    echo "%{F#1ABB9B}BATRI%{F-} -$bat0%" 
   fi
 fi
