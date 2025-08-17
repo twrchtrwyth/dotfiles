1ee2f14b29c30ed8f8bddbc1c3a763006276d26e
Wil Ifan Evans <w.evans@posteo.net>
2025-08-17 12:29:19 +0100

    Minor edits.

--- polybar/scripts/bat-dropdown-rofi.sh
+++ polybar/scripts/bat-dropdown-rofi.sh
@@ -13,7 +13,12 @@ STATE1=$(cat /sys/class/power_supply/BAT0/status)
 STATE2=$(cat /sys/class/power_supply/BAT1/status)
 
 # Format battery information for rofi
-BATTERY_INFO="Internal: $BATTERY1% ($STATE1)\n   Slice: $BATTERY2% ($STATE2)"
+# -d checks if a dir exists
+if [ -d /sys/class/power_supply/BAT1/ ]; then # BAT1 = external battery
+	BATTERY_INFO="Mewnol: $BATTERY1% ($STATE1)\n Sleis: $BATTERY2% ($STATE2)"
+else
+	BATTERY_INFO="Mewnol: $BATTERY1% ($STATE1)\n Sleis: NOT CONNECTED"
+fi
 
 # Display rofi popup with battery information
-echo -e "$BATTERY_INFO" | rofi -config ~/.config/rofi/config-bat.rasi -theme "$rofi_theme" -dmenu -p "Battery Status"
+echo -e "$BATTERY_INFO" | rofi -config ~/.config/rofi/config-bat.rasi -theme "$rofi_theme" -dmenu -p "Statws Batri"
--- polybar/scripts/bat-status-bar.sh
+++ polybar/scripts/bat-status-bar.sh
@@ -8,39 +8,48 @@ if [ -d /sys/class/power_supply/BAT1/ ]; then
 fi
 
 # Get the charging status of each battery
-status1=$(cat /sys/class/power_supply/BAT0/status)
+status0=$(cat /sys/class/power_supply/BAT0/status)
 if [ -d /sys/class/power_supply/BAT1/ ]; then
-  status2=$(cat /sys/class/power_supply/BAT1/status)
+  status1=$(cat /sys/class/power_supply/BAT1/status)
 fi
 
 # Check if any of the batteries are charging
-charging=false
-if [ "$status1" == "Charging" ] || [ "$status2" == "Charging" ]; then
-  charging=true
+charging0=false
+charging1=false
+if [ "$status0" == "Charging" ]; then
+  charging0=true
+elif [ "$status1" == "Charging" ]; then
+  charging1=true
 fi
 
 # Get the AC adapter status
 ac_status=$(cat /sys/class/power_supply/AC/online)
 
-# Show stats for the slice battery if connected and charged
+# Show stats for the slice battery if connected and has charge
 if [ -d /sys/class/power_supply/BAT1/ ] && [ "$bat1" -gt 0 ]; then
-  if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
-    echo "%{F#1ABB9B}BATRI%{F-} +$bat1%" 
-  elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
-    echo "%{F#1ABB9B}BATRI%{F-} LLAWN"
+  if [ "$charging1" == true ] && [ "$ac_status" == 1 ]; then
+    echo "%{F#1ABB9B}SLEIS%{F-} +$bat1%"
+  # If AC connected but neither battery is charging, they are both full
+  elif [ "$charging0" == false ] && [ "$charging1" == false ] && [ "$ac_status" == 1 ]; then
+    echo "%{F#1ABB9B}BATRIS%{F-} LLAWN"
+  # If the internal battery is charging, show that instead
+  elif [ "$charging0" == true ] && [ "$charging1" == false ] && [ "$ac_status" == 1 ]; then
+    echo "%{F#1ABB9B}BATRI%{F-} +$bat0%"
+  # If slice battery is low then show warning in red
   elif [ "$bat1" -lt 16 ]; then
-    echo "%{F#BC4028}BATRI ISEL%{F-} +$bat1%"
+    echo "%{F#BC4028}SLEIS ISEL%{F-} -$bat1%"
+  # Otherwise the slice battery is discharging
   else
-    echo "%{F#1ABB9B}BATRI%{F-} +$bat1%" 
+    echo "%{F#1ABB9B}SLEIS%{F-} -$bat1%" 
   fi
-# Otherwise just show status of internal battery
+# If no slice battery connected or it is dead, show status of internal battery
 else
-  if [ "$charging" == true ] && [ "$ac_status" == 1 ]; then
+  if [ "$charging0" == true ] && [ "$ac_status" == 1 ]; then
     echo "%{F#1ABB9B}BATRI%{F-} +$bat0%" 
-  elif [ "$charging" == false ] && [ "$ac_status" == 1 ]; then
-    echo "%{F#1ABB9B}BATRI%{F-} LLAWM"
+  elif [ "$charging0" == false ] && [ "$charging1" == false ] && [ "$ac_status" == 1 ]; then
+    echo "%{F#1ABB9B}BATRI%{F-} LLAWN"
   elif [ "$bat0" -lt 16 ]; then
-    echo "%{F#BC4028}BATRI ISEL%{F-} +$bat0%"
+    echo "%{F#BC4028}BATRI ISEL%{F-} -$bat0%"
   else
     echo "%{F#1ABB9B}BATRI%{F-} -$bat0%" 
   fi
